479d849d75b8aba5550725273bfba1dec4331783
GouthamKB <goutham79007@gmail.com>
2024-12-12 20:13:12 +0530

    changed relative paths in all pages

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -1,75 +1,82 @@
 const facilities = [
-    {
-        name: "Badminton",
-        description: "Our state-of-the-art badminton courts provide the perfect environment for both casual and competitive play. Enjoy a game with friends or join our tournaments. The courts are well-maintained and equipped with modern amenities to ensure a great playing experience.",
-        image: "assets/facilities/badminton.webp",
-        icon: "assets/icons/badminton.svg"
-    },
-    {
-        name: "Snooker",
-        description: "Relax and enjoy a game of snooker in our well-equipped snooker room. Perfect for both beginners and seasoned players, our snooker tables are of professional quality, ensuring a smooth and enjoyable game every time.",
-        image: "assets/facilities/snooker.webp",
-        icon: "assets/icons/snooker.svg"
-    },
-    {
-        name: "Gym",
-        description: "Our modern gym is equipped with the latest fitness equipment to help you stay in shape. Personal trainers are available for guidance, and we offer a variety of fitness classes to suit all levels. The gym is spacious and well-ventilated.",
-        image: "assets/facilities/gym.webp",
-        icon: "assets/icons/kettlebell.svg"
-    },
-    {
-        name: "Waiting Area",
-        description: "A comfortable waiting area with plush seating and a calm ambiance. Ideal for relaxing or waiting for your turn at the facilities. The area is designed to provide a peaceful environment with access to refreshments and reading materials.",
-        image: "assets/facilities/waiting-area.webp",
-        icon: "assets/icons/living-room.svg"
-    },
-    {
-        name: "Swimming Pool",
-        description: "Dive into our pristine swimming pool. Whether for a refreshing swim or a relaxing dip, our pool is perfect for all ages. The pool area is equipped with loungers and shaded areas, making it a great spot to unwind.",
-        image: "assets/facilities/pool.webp",
-        icon: "assets/icons/swimming-pool.svg"
-    },
-    {
-        name: "Permit Room",
-        description: "Our permit room offers a selection of beverages in a cozy setting. Perfect for unwinding after a long day, the room is designed to provide a relaxing atmosphere with comfortable seating and a variety of drinks to choose from.",
-        image: "assets/facilities/permit-room.webp",
-        icon: "assets/icons/chair.svg"
-    },
-    {
-        name: "Private Room",
-        description: "Enjoy privacy and comfort in our well-appointed private rooms. Ideal for meetings, small gatherings, or personal relaxation. Each room is equipped with modern amenities to ensure a comfortable and productive stay.",
-        image: "assets/facilities/rooms.webp",
-        icon: "assets/icons/living-room.svg"
-    }
+  {
+    name: "Badminton",
+    description:
+      "Our state-of-the-art badminton courts provide the perfect environment for both casual and competitive play. Enjoy a game with friends or join our tournaments. The courts are well-maintained and equipped with modern amenities to ensure a great playing experience.",
+    image: ".assets/facilities/badminton.webp",
+    icon: ".assets/icons/badminton.svg",
+  },
+  {
+    name: "Snooker",
+    description:
+      "Relax and enjoy a game of snooker in our well-equipped snooker room. Perfect for both beginners and seasoned players, our snooker tables are of professional quality, ensuring a smooth and enjoyable game every time.",
+    image: ".assets/facilities/snooker.webp",
+    icon: ".assets/icons/snooker.svg",
+  },
+  {
+    name: "Gym",
+    description:
+      "Our modern gym is equipped with the latest fitness equipment to help you stay in shape. Personal trainers are available for guidance, and we offer a variety of fitness classes to suit all levels. The gym is spacious and well-ventilated.",
+    image: ".assets/facilities/gym.webp",
+    icon: ".assets/icons/kettlebell.svg",
+  },
+  {
+    name: "Waiting Area",
+    description:
+      "A comfortable waiting area with plush seating and a calm ambiance. Ideal for relaxing or waiting for your turn at the facilities. The area is designed to provide a peaceful environment with access to refreshments and reading materials.",
+    image: ".assets/facilities/waiting-area.webp",
+    icon: ".assets/icons/living-room.svg",
+  },
+  {
+    name: "Swimming Pool",
+    description:
+      "Dive into our pristine swimming pool. Whether for a refreshing swim or a relaxing dip, our pool is perfect for all ages. The pool area is equipped with loungers and shaded areas, making it a great spot to unwind.",
+    image: ".assets/facilities/pool.webp",
+    icon: ".assets/icons/swimming-pool.svg",
+  },
+  {
+    name: "Permit Room",
+    description:
+      "Our permit room offers a selection of beverages in a cozy setting. Perfect for unwinding after a long day, the room is designed to provide a relaxing atmosphere with comfortable seating and a variety of drinks to choose from.",
+    image: ".assets/facilities/permit-room.webp",
+    icon: ".assets/icons/chair.svg",
+  },
+  {
+    name: "Private Room",
+    description:
+      "Enjoy privacy and comfort in our well-appointed private rooms. Ideal for meetings, small gatherings, or personal relaxation. Each room is equipped with modern amenities to ensure a comfortable and productive stay.",
+    image: ".assets/facilities/rooms.webp",
+    icon: ".assets/icons/living-room.svg",
+  },
 ];
 
 const renderSection = (item) => {
-    const section = document.createElement('section');
-    section.className = 'section';
+  const section = document.createElement("section");
+  section.className = "section";
 
-    const container = document.createElement("div");
-    container.className = "container";
+  const container = document.createElement("div");
+  container.className = "container";
 
-    const img = document.createElement('img');
-    img.src = item.image;
-    img.alt = '';
+  const img = document.createElement("img");
+  img.src = item.image;
+  img.alt = "";
 
-    const content = document.createElement("div");
+  const content = document.createElement("div");
 
-    const h3 = document.createElement('h3');
-    const p = document.createElement('p');
-    const icon = document.createElement("img");
-    h3.textContent = item.name;
-    p.textContent = item.description;
-    icon.src = item.icon;
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  const icon = document.createElement("img");
+  h3.textContent = item.name;
+  p.textContent = item.description;
+  icon.src = item.icon;
 
-    content.append(icon, h3, p);
-    container.append(content, img)
-    section.appendChild(container);
+  content.append(icon, h3, p);
+  container.append(content, img);
+  section.appendChild(container);
 
-    return section;
-}
+  return section;
+};
 
-facilities.forEach(facility => {
-    document.querySelector("#main").appendChild(renderSection(facility));
-}) 
+facilities.forEach((facility) => {
+  document.querySelector("#main").appendChild(renderSection(facility));
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,96 @@
-fetch('scripts/nearby-attractions.json')
-.then(response => response.json())
-.then(data => {
+fetch("scripts/nearby-attractions.json")
+  .then((response) => response.json())
+  .then((data) => {
     const attractions = data;
-    document.querySelectorAll(".attractions-list").forEach(element => {
-        attractions.forEach(attraction => {
-            const child = document.createElement("li");
-            child.innerHTML = attraction.attraction;
-            element.appendChild(child);
-        });
+    document.querySelectorAll(".attractions-list").forEach((element) => {
+      attractions.forEach((attraction) => {
+        const child = document.createElement("li");
+        child.innerHTML = attraction.attraction;
+        element.appendChild(child);
+      });
     });
-})
-.catch(error => console.error('Error fetching the JSON file:', error));
+  })
+  .catch((error) => console.error("Error fetching the JSON file:", error));
 
 function createEventSlide(event) {
-    const li = document.createElement("li");
-    li.className = "event-slide-container";
+  const li = document.createElement("li");
+  li.className = "event-slide-container";
 
-    const divSlide = document.createElement("div");
-    divSlide.className = "event-slide mobile-collapse";
+  const divSlide = document.createElement("div");
+  divSlide.className = "event-slide mobile-collapse";
 
-    const img = document.createElement("img");
-    img.src = event.images[0];
-    img.alt = "";
+  const img = document.createElement("img");
+  img.src = event.images[0];
+  img.alt = "";
 
-    const divDetails = document.createElement("div");
-    divDetails.className = "event-details";
+  const divDetails = document.createElement("div");
+  divDetails.className = "event-details";
 
-    const divTitle = document.createElement("div");
-    divTitle.className = "event-title";
+  const divTitle = document.createElement("div");
+  divTitle.className = "event-title";
 
-    const h2 = document.createElement("h2");
-    h2.textContent = event.heading;
+  const h2 = document.createElement("h2");
+  h2.textContent = event.heading;
 
-    const divDateCard = document.createElement("div");
-    divDateCard.className = "date-card";
+  const divDateCard = document.createElement("div");
+  divDateCard.className = "date-card";
 
-    const h6 = document.createElement("h6");
-    h6.textContent = event.date.split(" ")[1];
+  const h6 = document.createElement("h6");
+  h6.textContent = event.date.split(" ")[1];
 
-    const h3 = document.createElement("h3");
-    h3.textContent = event.date.split(" ")[2];
+  const h3 = document.createElement("h3");
+  h3.textContent = event.date.split(" ")[2];
 
-    const p = document.createElement("p");
-    p.textContent = event.description;
+  const p = document.createElement("p");
+  p.textContent = event.description;
 
-    divDateCard.append(h6, h3);
-    divTitle.append(h2, divDateCard);
-    divDetails.append(divTitle, p);
-    divSlide.append(img, divDetails);
-    li.appendChild(divSlide);
+  divDateCard.append(h6, h3);
+  divTitle.append(h2, divDateCard);
+  divDetails.append(divTitle, p);
+  divSlide.append(img, divDetails);
+  li.appendChild(divSlide);
 
-    return li;
+  return li;
 }
 
-fetch('scripts/latest-events.json')
-.then(response => response.json())
-.then(data => {
+fetch("scripts/latest-events.json")
+  .then((response) => response.json())
+  .then((data) => {
     const events = data;
-    for(let i=0; i<6; i++)
-        document.querySelector("#event-list").appendChild(createEventSlide(events[i]));
-})
-.catch(error => console.error('Error fetching the JSON file:', error));
-
-
+    for (let i = 0; i < 6; i++)
+      document
+        .querySelector("#event-list")
+        .appendChild(createEventSlide(events[i]));
+  })
+  .catch((error) => console.error("Error fetching the JSON file:", error));
 
 $(document).ready(function () {
+  setTimeout(
+    () =>
+      $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        items: 1,
+        center: true,
+      }),
+    300
+  );
 
-    setTimeout(() => $(".owl-carousel").owlCarousel({ loop: true, autoplay: true, items: 1, center: true }), 300)
+  const slideSRC = [
+    ".assets/index/slides/slide-1.webp",
+    ".assets/index/slides/slide-2.jpg",
+  ];
+  const slideCount = slideSRC.length;
+  let slideNumber = -1;
+  const slideTime = 16000;
 
-    const slideSRC = ["assets/index/slides/slide-1.webp", "assets/index/slides/slide-2.jpg"];
-    const slideCount = slideSRC.length;
-    let slideNumber = -1;
-    const slideTime = 16000;
-
-    function autoChangeSlide() {
-        (slideNumber < slideCount - 1) ? slideNumber++ : slideNumber = 0;
-        $("#slide-image-over").attr("src", slideSRC[slideNumber]);
-        setTimeout(() => {
-            $("#slide-image").attr("src", slideSRC[slideNumber])
-        }, slideTime / 2);
-    }
-    autoChangeSlide();
-    setInterval(autoChangeSlide, slideTime);
-})
+  function autoChangeSlide() {
+    slideNumber < slideCount - 1 ? slideNumber++ : (slideNumber = 0);
+    $("#slide-image-over").attr("src", slideSRC[slideNumber]);
+    setTimeout(() => {
+      $("#slide-image").attr("src", slideSRC[slideNumber]);
+    }, slideTime / 2);
+  }
+  autoChangeSlide();
+  setInterval(autoChangeSlide, slideTime);
+});
